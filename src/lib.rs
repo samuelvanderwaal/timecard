2096a9a8be867292de1c7c1bf5a5f6f0fd0dc447
Samuel J Vanderwaal <samuel.vanderwaal@gmail.com>
2019-12-06 23:53:45 -0900

    feat(lib, main, build): Write add various functions for parsing and pulling entries.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate lazy_static;
 
+use chrono::{Date, Datelike, Duration, Local};
+use diesel::dsl;
 use diesel::prelude::*;
-use diesel::sqlite::SqliteConnection;
 use diesel::result::Error as DieselError;
+use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
 // use chrono::{Local, DateTime, Datelike, Timelike};
 
@@ -12,7 +18,8 @@ pub mod schema;
 
 use schema::entries;
 
-#[derive(Debug, Clone, Queryable)]
+#[derive(Debug, Clone, Queryable, QueryableByName, Deserialize)]
+#[table_name = "entries"]
 pub struct Entry {
     pub id: i32,
     pub start: String,
@@ -22,7 +29,7 @@ pub struct Entry {
 }
 
 #[derive(Debug, Clone, Insertable)]
-#[table_name="entries"]
+#[table_name = "entries"]
 pub struct NewEntry {
     pub start: String,
     pub stop: String,
@@ -30,13 +37,26 @@ pub struct NewEntry {
     pub memo: String,
 }
 
+lazy_static! {
+    static ref WEEKDAYS: HashMap<String, i64> = vec![
+        ("Sun".to_owned(), 0),
+        ("Mon".to_owned(), 1),
+        ("Tue".to_owned(), 2),
+        ("Wed".to_owned(), 3),
+        ("Thu".to_owned(), 4),
+        ("Fri".to_owned(), 5),
+        ("Sat".to_owned(), 6),
+    ]
+    .into_iter()
+    .collect();
+}
+
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("Database url must be set!");
+    let database_url = env::var("DATABASE_URL").expect("Database url must be set!");
     SqliteConnection::establish(&database_url)
-            .expect(&format!("Error connecting to {}", database_url))
+        .expect(&format!("Error connecting to {}", database_url))
 }
 
 // pub fn write_entry(conn: &SqliteConnection, new_entry: &NewEntry) -> bool {
@@ -46,8 +66,23 @@ pub fn establish_connection() -> SqliteConnection {
 //         .is_ok()
 // }
 
+// Debug
 pub fn write_entry(conn: &SqliteConnection, new_entry: &NewEntry) -> Result<usize, DieselError> {
     diesel::insert_into(entries::table)
         .values(new_entry)
         .execute(conn)
+}
+
+pub fn entries_this_week(conn: &SqliteConnection) -> Vec<Entry> {
+    let week_day: String = Local::today().weekday().to_string();
+    let offset = *WEEKDAYS.get(&week_day).expect("Day does not exist!");
+    let week_beginning = Local::today() - Duration::days(offset);
+    println!("{:?}", week_beginning);
+    let week_entries: Vec<Entry> = dsl::sql_query(format!(
+        "SELECT * FROM entries WHERE start > '{}'",
+        week_beginning
+    ))
+    .load(conn)
+    .unwrap();
+    week_entries
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,78 @@
-use clap::{Arg, App};
+use chrono::{Datelike, Local};
+use clap::{App, Arg};
+use diesel::sqlite::SqliteConnection;
 
-use timecard::{establish_connection, NewEntry, write_entry};
+use timecard::{establish_connection, entries_this_week, write_entry, Entry, NewEntry};
 
 fn main() {
     let conn = establish_connection();
 
     let matches = App::new("timecard")
-                    .version("0.1.0")
-                    .author("Samuel Vanderwaal")
-                    .about("Time tracking command line program")
-                    .arg(Arg::with_name("entry")
-                        .short("e")
-                        .long("entry")
-                        .value_names(&["start", "stop", "code", "memo"])
-                        .help("Add a new time entry.")
-                        .takes_value(true)
-                        .value_delimiter(","))
-                    .arg(Arg::with_name("test")
-                        .short("t"))
-                    .get_matches();
+        .version("0.1.0")
+        .author("Samuel Vanderwaal")
+        .about("Time tracking command line program")
+        .arg(
+            Arg::with_name("entry")
+                .short("e")
+                .long("entry")
+                .value_names(&["start", "stop", "code", "memo"])
+                .help("Add a new time entry.")
+                .takes_value(true)
+                .value_delimiter(","),
+        )
+        .arg(Arg::with_name("test").short("t"))
+        .get_matches();
 
-    if let Some(values) = matches.values_of("entry")  {
-        let values: Vec<&str> = values.collect();
-        let start = values[0].to_owned();
-        let stop = values[1].to_owned();
-        let code = values[2].to_owned();
-        let memo = values[3].to_owned();
-        let new_entry = NewEntry { start, stop, code, memo };
-
-        // if !write_entry(&conn, &new_entry) {
-        //     println!("Failed to write entry: {:?}", new_entry);
-        // }
-        match write_entry(&conn, &new_entry) {
-            Ok(_) => (),
-            Err(e) => println!("Error writing entry: {:?}", e),
-        }
+    if let Some(values) = matches.values_of("entry") {
+        process_new_entry(values.collect(), &conn);
     }
+
+    let _entries = entries_this_week(&conn);
+}
+
+fn process_new_entry(values: Vec<&str>, conn: &SqliteConnection) {
+    let now = Local::now();
+    let year = now.year();
+    let month = now.month();
+    let day = now.day();
+
+    let (start_hour, start_minute) = parse_entry_time(values[0].to_owned());
+    let (stop_hour, stop_minute) = parse_entry_time(values[1].to_owned());
+
+    let start = format!(
+        "{}-{:02}-{:02} {:02}:{:02}:{:02}",
+        year, month, day, start_hour, start_minute, 0
+    );
+    let stop = format!(
+        "{}-{:02}-{:02} {:02}:{:02}:{:02}",
+        year, month, day, stop_hour, stop_minute, 0
+    );
+
+    let code = values[2].to_owned();
+    let memo = values[3].to_owned();
+
+    let new_entry = NewEntry {
+        start,
+        stop,
+        code,
+        memo,
+    };
+
+    // if !write_entry(&conn, &new_entry) {
+    //     println!("Failed to write entry: {:?}", new_entry);
+    // }
+    match write_entry(conn, &new_entry) {
+        Ok(_) => (),
+        Err(e) => println!("Error writing entry: {:?}", e),
+    }
+}
+
+fn parse_entry_time(time_str: String) -> (u32, u32) {
+    let time = time_str.parse::<u32>().expect("Failed to parse time!");
+    (time / 100, time % 100)
+}
+
+fn _create_weekly_report(entries: Vec<Entry>) -> String {
+    // Loop through entries, sum time for each project.
+    String::from("new")
 }
