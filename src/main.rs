8c35b3780b9eed943fb2579c346f757481b74701
Samuel J Vanderwaal <samuel.vanderwaal@gmail.com>
2020-05-26 10:34:41 -0800

    test(api): add more unit tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::convert::Infallible;
 use warp::{http, Filter};
 use warp::reply::Reply;
 use anyhow::Result;
-use serde_json;
 
 mod db;
 
@@ -18,7 +17,9 @@ async fn main() -> Result<()> {
 }
 
 async fn run(pool: SqlitePool) {
-    let routes = post_entry(pool.clone()).or(get_entry(pool));
+    let routes = post_entry(pool.clone())
+                    .or(get_entry(pool.clone()))
+                    .or(update_entry(pool));
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
@@ -49,11 +50,26 @@ fn get_entry(pool: SqlitePool) -> impl Filter<Extract = impl warp::Reply, Error 
         .and_then(read_entry)
 }
 
+fn update_entry(pool: SqlitePool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::post()
+        .and(warp::path("update_entry"))
+        .and(json_body())
+        .and(with_pool(pool))
+        .and_then(update_entry_handler)
+}
+
+fn delete_entry(pool: SqlitePool) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::post()
+        .and(warp::path("delete_entry"))
+        .and(warp::path::param::<i32>())
+        .and(with_pool(pool))
+        .and_then(delete_entry_handler)
+}
 
 // Handlers
 async fn new_entry(entry: Entry, pool: SqlitePool) -> Result<impl warp::Reply, Infallible> {
     match db::write_entry(&pool, &entry).await {
-        Ok(_) => return Ok(http::StatusCode::CREATED),
+        Ok(_) => return Ok(http::StatusCode::OK),
         Err(_) => return Ok(http::StatusCode::BAD_REQUEST)
     };
 }
@@ -72,11 +88,32 @@ async fn read_entry(id: i32, pool: SqlitePool) -> Result<warp::reply::Response, 
     }
 }
 
+async fn update_entry_handler(entry: Entry, pool: SqlitePool) -> Result<impl warp::Reply, Infallible> {
+    match db::update_entry(&pool, &entry).await {
+        Ok(_) => return Ok(http::StatusCode::OK),
+        Err(_) => return Ok(http::StatusCode::BAD_REQUEST)
+    }
+}
+
+async fn delete_entry_handler(id: i32, pool: SqlitePool) -> Result<impl warp::Reply, Infallible> {
+    match db::delete_entry(&pool, id).await {
+        Ok(_) => return Ok(warp::reply::with_status(
+            "Entry deleted.",
+            http::StatusCode::OK)
+            ),
+        Err(_) => return Ok(warp::reply::with_status(
+            "Error deleting entry.",
+            http::StatusCode::BAD_REQUEST)
+            )   
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use fake::{Fake, Faker};
     use bytes::Bytes;
+    use serde_json;
 
     #[tokio::test]
     async fn test_get_entry() -> Result<()> {
@@ -122,11 +159,70 @@ mod tests {
             .body(&exp_json)
             .reply(&filter).await;
 
-        assert_eq!(res.status(), 201);
+        assert_eq!(res.status(), 200);
 
         let entry = db::read_entry(&pool, exp_entry.id.unwrap()).await?;
 
         assert_eq!(&entry, &exp_entry);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_update_entry() -> Result<()> {
+        let pool = db::tests::setup_test_db().await?;
+        db::tests::setup_entries_table(&pool).await?;
+
+        let mut exp_entry: db::Entry = Faker.fake();
+        let id = db::write_entry(&pool, &exp_entry).await?;
+
+        exp_entry.id = Some(id);
+        exp_entry.start = String::from("0900");
+        exp_entry.stop = String::from("1100");
+        exp_entry.code = String::from("20-008");
+        exp_entry.memo = String::from("work, work, work");
+
+        let exp_json = Bytes::from(serde_json::to_string(&exp_entry).unwrap());
+
+        let filter = update_entry(pool.clone());
+
+        let res = warp::test::request()
+            .method("POST")
+            .path("/update_entry")
+            .body(&exp_json)
+            .reply(&filter).await;
+
+        assert_eq!(res.status(), 200);
+
+        let entry = db::read_entry(&pool, exp_entry.id.unwrap()).await?;
+
+        assert_eq!(&entry, &exp_entry);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_delete_entry() -> Result<()> {
+        let pool = db::tests::setup_test_db().await?;
+        db::tests::setup_entries_table(&pool).await?;
+
+        let mut entry: db::Entry = Faker.fake();
+        entry.id = Some(1);
+        db::write_entry(&pool, &entry).await?;
+        
+        let filter = delete_entry(pool.clone());
+
+        let res = warp::test::request()
+            .method("POST")
+            .path("/delete_entry/1")
+            .reply(&filter).await;
+
+        assert_eq!(res.status(), 200);
+
+        // Entry should not exist.
+        let _ = db::read_entry(&pool, entry.id.unwrap()).await.is_err();
+
+        assert_eq!(res.body(), "Entry deleted.");
 
         Ok(())
     }
